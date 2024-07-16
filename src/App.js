9a42436d4ce0c415a84cd44c82870bb84870dc74
hyojoo <jhj080802@naver.com>
2024-07-16 17:04:29 +0900

    컴포넌트 사용

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <li>
+        <a href="/read/1">html</a>
+      </li>
+      <li>
+        <a href="/read/2">css</a>
+      </li>
+      <li>
+        <a href="/read/3">js</a>
+      </li>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <h1>
-          <a href="/">WEB</a>
-        </h1>
-      </header>
-      <nav>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Header />
+      <Nav />
+      <Article />
     </div>
   );
 }
