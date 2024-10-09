c40a00f9e123b6e1ce2695616ab30240b9b30402
hyojoo <jhj080802@naver.com>
2024-10-09 16:00:58 +0900

    add array

--- src/UserList.js
+++ src/UserList.js
@@ -1,38 +1,15 @@
+// UserList.js
 import React from "react";
 
-function User({ user }) {
+function UserList({ users }) {
   return (
-    <div>
-      <b>{user.username}</b> <span>({user.email})</span>
-    </div>
-  );
-}
-
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
-
-  return (
-    <div>
-      <User user={users[0]} />
-      <User user={users[1]} />
-      <User user={users[2]} />
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.username} ({user.email})
+        </li>
+      ))}
+    </ul>
   );
 }
 
