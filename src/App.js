cd4867bf73acc41144f7f007e89c774ad1e3a5ef
hyojoo <jhj080802@naver.com>
2024-10-10 17:48:29 +0900

    delete array element - dont

--- src/App.js
+++ src/App.js
@@ -3,27 +3,58 @@ import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
 function App() {
-  const [inputs, setInputs] = useState({ username: "", email: "" });
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
   const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
 
   const [users, setUsers] = useState([
-    { id: 1, username: "velopert", email: "public.velopert@gmail.com" },
-    { id: 2, username: "tester", email: "tester@example.com" },
-    { id: 3, username: "liz", email: "liz@example.com" },
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
   ]);
 
   const nextId = useRef(4);
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: value });
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
   };
 
-  const onCreate = () => {
-    const user = { id: nextId.current, username, email };
-    setUsers([...users, user]); // spread 연산자를 사용해 새로운 배열 생성
-    setInputs({ username: "", email: "" });
-    nextId.current += 1;
+  const onRemove = (id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter((user) => user.id !== id));
   };
 
   return (
@@ -34,7 +65,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,15 +1,21 @@
-// UserList.js
 import React from "react";
 
-function UserList({ users }) {
+function User({ user, onRemove }) {
   return (
-    <ul>
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+}
+
+function UserList({ users, onRemove }) {
+  return (
+    <div>
       {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.email})
-        </li>
+        <User user={user} key={user.id} onRemove={onRemove} />
       ))}
-    </ul>
+    </div>
   );
 }
 
